08028287f1a0a6c244ed79a70799e664809c5baf
Vishal Yadav <vishalyadav1k2b3c@gmail.com>
2024-09-03 00:57:45 +0530

    Update atlas_uri.js

--- atlas_uri.js
+++ atlas_uri.js
@@ -1,1 +1,1 @@
-module.exports = uri =  "mongodb+srv://vishalyadav1k2b3c:Vishalyadav%401@cluster0.kg7s5.mongodb.net/";
+module.exports = uri =  "mongodb+srv://username:password%401@cluster0.kg7s5.mongodb.net/";
